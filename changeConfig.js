5553098230e6cfd207624982a883a176ca0ecd9a
Matthias Huber <hubermat@gmx.de>
2018-07-27 13:29:05 +0200

    add comment option

--- changeConfig.js
+++ changeConfig.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 
 if ((process.argv.length < 3) || ((process.argv[2] === 'add') && (process.argv.length < 8))) {
   console.log('Usage:');
-  console.log('  node changeConfig.js add <sitename> <uri> <password> <api-user> <api-password>');
+  console.log('  node changeConfig.js add <sitename> <uri> <password> <api-user> <api-password> [<comment>]');
   console.log('  node changeConfig.js remove <sitename>');
   console.log('  node changeConfig.js show <sitename>');
   return;
@@ -15,6 +15,7 @@ var uri = process.argv[4];
 var password = process.argv[5];
 var apiUser = process.argv[6];
 var apiPassword = process.argv[7];
+var comment = process.argv[8];
 
 
 var config = ini.parse(fs.readFileSync('./ctldap.config', 'utf-8'));
@@ -24,6 +25,9 @@ if (command === 'show') {
     console.log('Site: ' + sitename);
     console.log('URI: ' + config.sites[sitename].ct_uri);
     console.log('LDAP root passsword: ' + config.sites[sitename].ldap_password);
+    if (config.sites[sitename].comment) {
+      console.log('Comment: '+config.sites[sitename].comment);
+    }
   } else {
     console.log('No LDAP config for site '+sitename+' .');
   }
@@ -40,7 +44,8 @@ if (command === 'add') {
     ldap_password: password,
     ct_uri: uri,
     api_user: apiUser,
-    api_password: apiPassword
+    api_password: apiPassword,
+    comment: comment
   };
   if (password.substr(0, 4) === '$2y$') {
     config.sites[sitename].ldap_password_bcrypt = true;
